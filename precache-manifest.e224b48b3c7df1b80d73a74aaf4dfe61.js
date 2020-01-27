319dfbb5c461d9a77d8281926dcd6dadb9ff1836
Yurii Kichalo <yuriykichalo@gmail.com>
2020-01-27 18:47:07 +0200

    Updates

---
+++ precache-manifest.e224b48b3c7df1b80d73a74aaf4dfe61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fc9f1fda5c0e9910978809c1b8eb30",
+    "url": "/googleCalendarTasks/index.html"
+  },
+  {
+    "revision": "5f68240f2f47eece0885",
+    "url": "/googleCalendarTasks/static/css/2.57125576.chunk.css"
+  },
+  {
+    "revision": "e01368f6e9fd8d065b36",
+    "url": "/googleCalendarTasks/static/css/main.385e5bb9.chunk.css"
+  },
+  {
+    "revision": "5f68240f2f47eece0885",
+    "url": "/googleCalendarTasks/static/js/2.740a93da.chunk.js"
+  },
+  {
+    "revision": "1b7230e03535fc39474324a8a11caaff",
+    "url": "/googleCalendarTasks/static/js/2.740a93da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e01368f6e9fd8d065b36",
+    "url": "/googleCalendarTasks/static/js/main.264e796f.chunk.js"
+  },
+  {
+    "revision": "20154898fce03ba11add",
+    "url": "/googleCalendarTasks/static/js/runtime-main.59d56d94.js"
+  }
+]);
